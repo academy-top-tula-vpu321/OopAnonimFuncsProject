aa5a13b28079b00822185fc8ec1a7482e0054be1
maxefimov <efimov.top.academy@gmail.com>
2023-12-17 11:44:35 +0300

    Добавьте файлы проекта.

---
+++ OopAnonimFuncsProject/OopAnonimFuncsProject.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <typeinfo>
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Mult(int a, int b)
+{
+    return a * b;
+}
+
+int Calc(int a, int b, int(*operation)(int, int))
+{
+    return operation(a, b);
+}
+
+
+int main()
+{
+    int (*oper)(int, int);
+    //oper = Sum;
+    std::cout << Calc(10, 20, Sum) << "\n";
+    
+    //oper = Mult;
+    std::cout << Calc(10, 20, Mult) << "\n";
+    
+    
+    //oper = ;
+
+    std::cout << Calc(10, 20, 
+        [](int a, int b)->int
+        {
+            return a - b;
+        }) << "\n";
+    //std::cout << typeid(oper).name() << "\n";
+
+
+    int number{ 20 };
+    int n1{ 5 };
+
+    auto f1 = [&](int a, int b)
+        {
+            number = 20;
+            n1 = 30;
+            return a + b + number + n1;
+        };
+}
